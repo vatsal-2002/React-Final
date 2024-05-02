bb42a2b09bd4d416e20257847a2c5bb0f3f593de
vatsal prajapati <vatsprajapati20@gmail.com>
2024-05-02 15:05:11 +0530

    create First react component

---
+++ reactbasic/src/Basic.js
@@ -0,0 +1,5 @@
+function Basic() {
+    return <h1>Hello vatsal</h1>
+}
+
+export default Basic;
--- reactbasic/src/index.js
+++ reactbasic/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Basic from './Basic';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Basic />
+  </>
 );
 
