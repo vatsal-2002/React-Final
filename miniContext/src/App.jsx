8817341201fbe0cfb05ec8641239cd7ec6a0c709
vatsal prajapati <vatsprajapati20@gmail.com>
2024-05-10 12:59:17 +0530

    Context API with Mini Project

---
+++ miniContext/src/App.jsx
@@ -0,0 +1,18 @@
+import UserContextProvider from './context/UserContextProvider'
+import './App.css'
+import Login from './component/Login'
+import Profile from './component/Profile'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>React Context API</h1>
+      <Login/>
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
